e2a17eaa3fe6909c838c3b5a6b0f3a3c86a74d57
emma.macias <emmamaciasdf@gmail.com>
2022-04-14 20:39:52 -0300

    Aplicando GRID

---
+++ js/disney.js
@@ -0,0 +1,17 @@
+let estrellas = document.getElementById('estrellas');
+let luna = document.getElementById('luna');
+let nubes = document.getElementById('nubes');
+let titulo = document.getElementById('titulo');
+let btn = document.getElementById('btn');
+let front = document.getElementById('front');
+
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    estrellas.style.left = value * 0.30 + 'px';
+    luna.style.top = value * 0.8 + 'px';
+    nubes.style.top = value * 0.3 + 'px';
+
+
+    
+}) 
